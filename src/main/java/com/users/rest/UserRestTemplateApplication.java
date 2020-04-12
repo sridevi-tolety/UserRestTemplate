ec5afaab6302be7cbd867d0ca9e7cba6a55f8ae3
unknown <sridevi.tolety15@gmail.com>
2020-04-12 19:38:53 +0530

    commit UserRestTemplate

---
+++ src/main/java/com/users/rest/UserRestTemplateApplication.java
@@ -0,0 +1,12 @@
+package com.users.rest;
+​
+import org.springframework.boot.SpringApplication;​
+​
+@SpringBootApplication​
+public class UserRestTemplateApplication {​
+​
+public static void main(String[] args) {​
+SpringApplication.run(UserRestTemplateApplication.class, args);​
+}​
+​
+}​
